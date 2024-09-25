47aa0763d76f7978d9acb256fc51c07e6ccc27b3
odofin-timothy <68248727+timothy-odofin@users.noreply.github.com>
2024-09-25 22:34:10 +0100

    Restructured the project

--- app/gateway/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import CustomTokenObtainPairView
-from app.app_url import AppUri
-
-urlpatterns = [
-    path(AppUri.LOGIN.uri, CustomTokenObtainPairView.as_view(), name=AppUri.LOGIN.uri_name)
-]
